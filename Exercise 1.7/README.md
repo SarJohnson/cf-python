8b91460438261b5165aeb0de0d3e1f88a29484b6
SarJohnson <sej17b@my.fsu.edu>
2024-01-14 19:28:27 -0500

    Completed task 1.7

---
+++ Exercise 1.7/README.md
@@ -0,0 +1,14 @@
+# Exercise 1.7: Object-Relational Mapping in Python
+
+## Step 1: 
+- Set up SQLAlchemy & recipe_app.py script
+## Step 2: 
+- Create Recipe model and declarative base class Base
+## Step 3: 
+- Define create_recipe, view_all_recipes, search_by_ingredient, edit_recipe, & delete_recipe
+## Step 4: 
+- Create main menu contained in while loop
+## Step 5: 
+- Update a Recipe with update_recipe()
+## Step 6: 
+- Create recipes and run script
---
+++ Exercise 1.7/recipe_app.py
@@ -0,0 +1,220 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.types import Integer, String
+from sqlalchemy import Column
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+mysqlconnector://cf-python:password@localhost/tasks_database")
+
+Base = declarative_base()
+
+class Recipe(Base):
+    __tablename__ = 'final_recipes_table'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50))
+    ingredients = Column(String(255))
+    cooking_time = Column(Integer)
+    difficulty = Column(String(20))
+
+    def __repr__(self):
+        return f"<Recipe(id={self.id}, name='{self.name}', difficulty='{self.difficulty}')>"
+
+    def __str__(self):   
+      output = "\nName: " + str(self.name) + \
+      "\nCooking time (minutes): " + str(self.cooking_time) + \
+      "\nDifficulty: " + str(self.difficulty) + \
+      "\nIngredients: " + str(self.ingredients)
+      return output
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def calc_difficulty(cooking_time, recipe_ingredients):
+    if (cooking_time <= 90) and (len(recipe_ingredients) <= 5):
+        difficulty_level = 'Easy'
+    elif (cooking_time <= 90) and (len(recipe_ingredients) > 5):
+        difficulty_level = 'Intermediate'
+    elif (cooking_time > 90) and (len(recipe_ingredients) < 10):
+        difficulty_level = 'Hard'
+    elif (cooking_time > 90) and (len(recipe_ingredients) >= 10):
+        difficulty_level = 'Expert'
+    else:
+        print("sorry there was an error. please try again.")
+    return difficulty_level
+        
+def create_recipe():
+    recipe_ingredients = []
+    name_input_value = False
+    cooking_time_input_value = False
+    ingredients_quantity = False
+    while name_input_value == False:
+        name = input(" what is the name for this Recipe? - ")
+        if len(name) < 50:
+            name_input_value = True   
+        else:
+            print("please enter a name with less than 50 characters")
+    while cooking_time_input_value == False:
+        cooking_time = input("how many minutes to make this recipe? - ")     
+        if cooking_time.isnumeric() == True:
+            cooking_time_input_value = True
+    while ingredients_quantity == False:
+        ingredients_input = input("how many ingredients would you like to add? - ")
+        if ingredients_input.isnumeric() == True:
+            ingredients_quantity = True
+            for _ in range(int(ingredients_input)):
+                ingredient = input("add the name of the ingredient - ")
+                recipe_ingredients.append(ingredient)
+        else: 
+            print("please enter a postive number")   
+    recipe_ingredients_str = ','.join(recipe_ingredients)
+    print(recipe_ingredients_str)
+    difficulty = calc_difficulty(int(cooking_time), recipe_ingredients)
+    recipe_entry = Recipe(
+        name = name,
+        cooking_time = int(cooking_time),
+        ingredients = recipe_ingredients_str,
+        difficulty = difficulty
+    )
+    session.add(recipe_entry)
+    session.commit()
+    print("recipe has been added to the database")
+    
+def view_all_recipes():
+    all_recipes = []
+    all_recipes = session.query(Recipe).all() 
+    if len(all_recipes) == 0:
+        print("sorry recipes not found")
+    else:
+        print("\nhere is a list of all the recipes")
+    for recipe in all_recipes:
+        print(recipe)
+                
+def search_by_ingredients():
+    check_recipes = session.query(Recipe).count()
+    if check_recipes == 0:
+        print("sorry there are no recipes in your database")
+        return None
+    else:
+        results = session.query(Recipe.ingredients).all()
+        all_ingredients = []
+        for recipe_ingredient_lists in results:
+            for recipe_ingredients in recipe_ingredient_lists:
+                recipe_ingedients_split = recipe_ingredients.split(',') 
+                all_ingredients.extend(recipe_ingedients_split) 
+        all_ingredients = list(dict.fromkeys(all_ingredients))
+        all_ingredients_list = list(enumerate(all_ingredients))
+        print("\nlist of all ingredients - ")
+        for index,tup in enumerate(all_ingredients_list):
+            print(str(tup[0]+1) + "." + tup[1])  
+        try:
+            ingredients_searched_num = input("\nEnter one or more numbers to select one or more ingredients - ")
+            ingredients_list_searched = ingredients_searched_num.split(" " or "")
+            searched_ingredients_list = []
+            for ingredients_searched_num in ingredients_list_searched:
+                ingredients_searched_index = int(ingredients_searched_num) - 1 
+                ingredients_searched = all_ingredients_list[ingredients_searched_index][1]
+                searched_ingredients_list.append(ingredients_searched)
+            print("selected ingredients - ", searched_ingredients_list)
+            conditions = []
+            for ingredient in searched_ingredients_list:
+                like_term = "%"+ingredient+"%"
+                condition = Recipe.ingredients.like(like_term)
+                conditions.append(condition)
+            searched_recipes = session.query(Recipe).filter(*conditions).all()
+        except:
+            print("Make sure to select a number from the list.")
+        else:
+            print("\nThe recipe(s) below include(s) the selected ingredient: ")
+            for recipe in searched_recipes:
+                print(recipe)
+
+def edit_recipe():
+    check_recipes = session.query(Recipe).count()
+    if check_recipes == 0:
+        print("no recipes in your database")
+        return None
+    else:
+        results = session.query(Recipe).with_entities(Recipe.id,Recipe.name).all()
+        print("recipes to update")
+        for recipe in results:
+            print("\nID - ", recipe[0])
+            print("\nname - ", recipe[1])
+        recipe_id_to_edit = int(input("please enter the id of the recipe you'd like to update - "))
+        recipe_id_tup_list = session.query(Recipe).with_entities(Recipe.id).all()
+        recipe_id_list = []
+        for recipe_tup in recipe_id_tup_list:
+            print(recipe_tup[0])
+            recipe_id_list.append(recipe_tup[0])
+        print(recipe_id_list)
+        if recipe_id_to_edit not in recipe_id_list:
+            print("sorry, couldn't find your recipe by id. please try again")
+        else:
+            recipe_to_edit = session.query(Recipe).filter(Recipe.id == recipe_id_to_edit).one()
+            print("\n you are about to edit the following recipe - ")
+            print(recipe_to_edit)       
+            column_for_update = int(input("\nEnter the data you want to update among 1. name, 2. cooking time 3. ingredients -  "))
+            updated_value = input("please enter the new value for your recipe - ")
+            if column_for_update == 1:
+                session.query(Recipe).filter(Recipe.id == recipe_id_to_edit).update({Recipe.name: updated_value})
+                print(column_for_update, " has been updated")
+            elif column_for_update == 2:
+                session.query(Recipe).filter(Recipe.id == recipe_id_to_edit).update({Recipe.cooking_time: updated_value})
+                print(column_for_update, " has been updated")
+            elif column_for_update == 3:
+                session.query(Recipe).filter(Recipe.id == recipe_id_to_edit).update({Recipe.ingredients: updated_value})
+            else:
+                print("\noops, something happened. please try again")
+            update_difficulty = calc_difficulty(int(recipe_to_edit.cooking_time), recipe_to_edit.ingredients)
+            recipe_to_edit.difficulty = update_difficulty
+            session.commit()
+            print("Your Recipe has been updated")
+
+def delete_recipe():
+        check_recipes = session.query(Recipe).count()
+        if check_recipes == 0:
+            print("no recipes in your database")
+            return None
+        else:
+            recipe_list = session.query(Recipe).with_entities(Recipe.id, Recipe.name).all()
+            print("list of recipes")
+            for recipe in recipe_list:
+                print("\nid: ", recipe[0])
+                print("\nname: ", recipe[1])
+            recipe_id_input = input("please enter the ID of the recipe you would like to delete - ")
+            recipe_to_delete = session.query(Recipe).filter(Recipe.id == recipe_id_input).one()
+            confirm_delete = input("are you sure you want to delete this recipe? yes / no - ")
+            if confirm_delete == 'yes':
+                session.delete(recipe_to_delete)
+                session.commit()
+                print("you have successfully deleted this recipe")    
+            else:
+                return None
+        
+while True:
+    print("1. Create a new recipe")
+    print("2. View all recipes")
+    print("3. Search for recipes by ingredients")
+    print("4. Edit a recipe")
+    print("5. Delete a recipe")
+    print("Type 'quit' to quit the application")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        create_recipe()
+    elif choice == "2":
+        view_all_recipes()
+    elif choice == "3":
+        search_by_ingredients()
+    elif choice == "4":
+        edit_recipe()
+    elif choice == "5":
+        delete_recipe()
+    elif choice.lower() == "quit":
+        break
+    else:
+        print("Invalid input. Please try again.")
+
+session.close()
+engine.dispose()
